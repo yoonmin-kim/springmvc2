40222e2a58d651f628c85adf2cc93c4d76a08645
YoonMin <rladbsals23@gmail.com>
2021-05-15 19:33:51 +0900

    feat : 요청 매핑 헨들러 어뎁터 구조
    DispatcherServlet
    -> ** RequestMapping **
    ->    ArgumentResolver <-> Http메세지 컨버터
    ->    핸들러(컨트롤러)
    ->    ReturnValueHandler <-> Http메세지 컨버터
    -> ** RequestMapping **
    
    [ArgumentResolver]
     - HttpServletRequest, Model, @RequestBody, @ModelAttribute, HttpEntity 와 같은
       Http 메세지를 처리하는 부분까지 파라미터를 유연하게 처리할 수 있게 해준다.
    
    [ReturnValueHandler]
     - ArgumentResolver와 비슷하게 응답값을 변환하고 처리한다.

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run( SpringmvcApplication.class, args);
+		SpringApplication.run(SpringmvcApplication.class, args);
 	}
 
 }
